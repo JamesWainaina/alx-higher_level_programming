fdc5de3098b9d617e05fe75a0d551f96f2c3a80f
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-02 17:53:49 +0300

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,12 +42,10 @@ class Rectangle:
     def area(self):
         """public instance area"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
         """public instance perimeter"""
         if self.__width == 0 or self.__height == 0:
             """ return the perimeter"""
             return 0
         return (self.__width * 2) + (self.__height * 2)
-
-

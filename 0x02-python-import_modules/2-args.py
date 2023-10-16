543b262a5c75b7b3806ea47278796717b0fcc7cc
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 19:45:11 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""print the no of arguments and the argument"""
 
-import sys
-
-argv = sys.argv[1:]
-num_args= len(argv)
-
-if num_args == 0:
-    print("0 argument.")
-    print(".")
-else:
-    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-    for i, arg in enumerate(argv , 1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    num_args= len(argv)
+    if num_args == 0:
+        print("0 argument.")
+        print(".")
+    else:
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+        for i, arg in enumerate(argv , 1):
+            print(f"{i}: {arg}")

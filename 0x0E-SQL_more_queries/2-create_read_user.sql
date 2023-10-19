73ca2c663da4f536d5b99b99e30d5ce59ae426c6
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-19 13:33:13 +0300

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
--- creating user and database.
--- user has select privileges.
-
+-- script to run msql commands
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED AS 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost

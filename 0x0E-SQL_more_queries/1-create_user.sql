67d216414262fef4b7f5f72a5c1212267d86775e
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-19 13:21:23 +0300

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- user_od_1 should have all privileges on your MySLQ server
 -- password set to user_od_1_pwd.
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 

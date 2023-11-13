aa314dc3ccc75fb187785b4e88cb90e6f9c8bce8
JamesWainaina <jamesgatheru9905@gmail.com>
2023-11-13 14:42:41 +0300

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// prints the message according to the argument passed.
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Argument found');

845d425f81bb1434569705e00ae6a30e69f4bf4a
JamesWainaina <jamesgatheru9905@gmail.com>
2023-11-13 16:40:26 +0300

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-  console.log('Mising size');
+  console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
     let row = '';

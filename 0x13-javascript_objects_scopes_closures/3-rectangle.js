a2d6bf71b890b93dc9e10ea97cef59a452c2f268
JamesWainaina <jamesgatheru9905@gmail.com>
2023-11-16 11:34:12 +0300

    semi standard fix task 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.heigth = h;
     }
@@ -17,5 +17,3 @@ class Rectangle {
     }
   }
 };
-
-module.exports = Rectangle;

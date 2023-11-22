04dddaecab255c5875837926e88b6560ce2bc5fe
JamesWainaina <jamesgatheru9905@gmail.com>
2023-11-22 17:02:11 +0300

    task 5 correct argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            password=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities \
-                INNER JOIN states ON cities.state_id = states.id \
-                WHERE states.name = '{}'\
-                ORDER BY cities.id ASC".format(argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if len(argv[4]) <= 12:
+        cur.execute("SELECT cities.name FROM cities \
+                    INNER JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = '{}'\
+                    ORDER BY cities.id ASC".format(argv[4]))
+        rows = cur.fetchall()
+        cities = ", ".join(row[0] for row in rows)
+        print(cities)
     cur.close()
     conn.close()

9c2504654382d9591b6034b93ccfc1733d53eb12
JamesWainaina <jamesgatheru9905@gmail.com>
2023-12-04 19:58:47 +0300

    correct format of task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 
 from urllib.request import urlopen
 
-if __name__== "__main__":
+if __name__ == "__main__":
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         de_html = html.decode()
         print("Body response:\n\t- type: {}\n\t- content: {}\n\
-              \t- utf8 content: {}".format(type(html),html, de_html))
+\t- utf8 content: {}".format(type(html), html, de_html))

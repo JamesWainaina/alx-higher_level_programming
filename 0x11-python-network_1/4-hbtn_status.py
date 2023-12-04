06c55d435062736ede111fd70cc30f3ba611043e
JamesWainaina <jamesgatheru9905@gmail.com>
2023-12-04 19:15:13 +0300

    using requests to get the content of a url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches 
+https://alx-intranet.hbtn.io/status
+You must use the package requests"""
+
+import requests
+
+if __name__ == "__main__":
+
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:\n\t- type: {}\n\t- \
+content: {}".format(type(req.text),req.text))

22c4bccf9650c4d7afa1d31b579c79eeb4083680
JamesWainaina <jamesgatheru9905@gmail.com>
2023-12-04 17:52:09 +0300

    fetching a url and printing its body response ,type and content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+from urllib.request import urlopen
+
+if __name__== "__main__":
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        de_html = html.decode()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\
+              \t- utf8 content: {}".format(type(html),html, de_html))

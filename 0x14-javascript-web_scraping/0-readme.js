c2a68520adcc214c80ce5bef6fb0bcdc969c5780
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-03 18:37:09 +0300

    semistandard --fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require ('fs');
+const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf-8', (err, data) =>{
-    if (err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });

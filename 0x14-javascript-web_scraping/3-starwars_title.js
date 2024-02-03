370586ab6310a73987b95d7abc8ca5197dc5907a
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-03 19:12:46 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,13 +4,13 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request.get(url, function (err, response, body){
-    if (err){
-        console.log(err);
-    }else{
-        if (response.statusCode === 200){
-            const result = JSON.parse(body);
-            console.log(result.title);
-        }
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    if (response.statusCode === 200) {
+      const result = JSON.parse(body);
+      console.log(result.title);
     }
+  }
 });
